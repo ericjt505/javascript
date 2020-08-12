5169823a60faf5f630b787d2c9c783647d388eb8
ericjt505 <weirdspacetobe@gmail.com>
2020-08-12 11:57:20 -0400

    update again

--- scripts.js
+++ scripts.js
@@ -1,1 +1,46 @@
 
+
+
+
+
+
+
+
+
+
+
+/*
+let myName;
+
+let myAge = 49;
+
+myName = 'Eric';
+myName = 'James';
+myAge = 40;
+alert(myAge);
+*/
+
+/*
+let test = 6<3;
+alert(test);
+*/
+
+/*
+let myNameArray = ['Eric', 'James', 'Tyrone'];
+let myNameerArray = [10, 15, 40];
+alert(myNameArray[0]);
+*/
+
+/*
+let dog = {name: 'Spot', breed: 'Dalmation'};
+alert(dog.breed);
+*/
+
+/*
+let myNumber = '500';
+alert(typeof myNumber); //produce a string
+
+let myNumber = 500;
+alert(typeof myNumber); //produce a number
+
+*/
